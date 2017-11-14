8e5cd724352d3e35a9b3d75b45a0c21fcd70899a
Said <saidziani.pro@gmail.com>
2017-11-14 08:10:54 +0100

    vectorial search methods.

---
+++ Vectorial.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python3
+
+import Indexation, Inverse, re, nltk, math
+from nltk.corpus import stopwords
+from operator import itemgetter
+
+class Vectorial():
+
+    def __init__(self, directory = False, query = False):
+        self.directory = directory
+        self.query = query
+
+
+    def getPropQuery(self):
+        query = self.query
+        ponctuation = re.compile('[^\w\s]?', re.IGNORECASE)
+        propQuery = re.sub(ponctuation, '', query)
+        propQuery = query.split()
+        propQuery = [word for word in propQuery if word not in stopwords.words('french')]
+        return propQuery, nltk.FreqDist(propQuery)
+
+
+    def innerProduct(self):
+        pond = Inverse.Inverse(self.directory).getPonderation()
+        propQuery, FreqDist = self.getPropQuery()
+        rsvDocs = []
+        for doc in pond:
+            RSV = 0
+            for docTuple in doc[1]:
+                if docTuple[0] in FreqDist.keys():
+                    word = docTuple[0]
+                    occ, freq = FreqDist[word], docTuple[1]
+                    RSV += occ * freq
+            rsvDocs.append((doc[0], round(float(RSV),3)))
+            rsvDocsSorted = sorted(rsvDocs, key=itemgetter(1), reverse=True)
+        return rsvDocsSorted
+
+
+    def diceCoef(self):
+        pond = Inverse.Inverse(self.directory).getPonderation()
+        propQuery, FreqDist = self.getPropQuery()
+        sumFreqDist = 0
+        for item in FreqDist.values():
+            powFreq = item * item
+            sumFreqDist += powFreq
+        rsvDocs = []
+        for doc in pond:
+            subRSV = 0
+            RSV = 0
+            sumPond = 0
+            for docTuple in doc[1]:
+                sumPond += math.pow(float(docTuple[1]), 2)
+                if docTuple[0] in FreqDist.keys():
+                    word = docTuple[0]
+                    occ, freq = FreqDist[word], docTuple[1]
+                    subRSV +=  (occ * freq)
+            RSV = (2 * subRSV) / (sumPond + sumFreqDist)
+            rsvDocs.append((doc[0], round(float(RSV),3)))
+            rsvDocsSorted = sorted(rsvDocs, key=itemgetter(1), reverse=True)
+        return rsvDocsSorted
+
+
+    def cosinusMesure(self):
+        pond = Inverse.Inverse(self.directory).getPonderation()
+        propQuery, FreqDist = self.getPropQuery()
+        sumFreqDist = 0
+        for item in FreqDist.values():
+            powFreq = item * item
+            sumFreqDist += powFreq
+        rsvDocs = []
+        for doc in pond:
+            subRSV = 0
+            RSV = 0
+            sumPond = 0
+            for docTuple in doc[1]:
+                sumPond += math.pow(float(docTuple[1]), 2)
+                if docTuple[0] in FreqDist.keys():
+                    word = docTuple[0]
+                    occ, freq = FreqDist[word], docTuple[1]
+                    subRSV +=  (occ * freq)
+            RSV = (subRSV) / math.sqrt(sumPond * sumFreqDist)
+            rsvDocs.append((doc[0], round(float(RSV),3)))
+            rsvDocsSorted = sorted(rsvDocs, key=itemgetter(1), reverse=True)
+        return rsvDocsSorted
+
+
+    def jaccardMesure(self):
+        pond = Inverse.Inverse(self.directory).getPonderation()
+        propQuery, FreqDist = self.getPropQuery()
+        sumFreqDist = 0
+        for item in FreqDist.values():
+            powFreq = item * item
+            sumFreqDist += powFreq
+        rsvDocs = []
+        for doc in pond:
+            subRSV = 0
+            RSV = 0
+            sumPond = 0
+            for docTuple in doc[1]:
+                sumPond += math.pow(float(docTuple[1]), 2)
+                if docTuple[0] in FreqDist.keys():
+                    word = docTuple[0]
+                    occ, freq = FreqDist[word], docTuple[1]
+                    subRSV +=  (occ * freq)
+            RSV = (subRSV) / (sumPond + sumFreqDist - subRSV)
+            rsvDocs.append((doc[0], round(float(RSV),3)))
+            rsvDocsSorted = sorted(rsvDocs, key=itemgetter(1), reverse=True)
+        return rsvDocsSorted
+
+
+
+
+
+
+# if __name__ == "__main__":
+#     a = "langage le python langage "
+    
+#     v = Vectorial('/media/said/DevStuff/Master2-Sii/RI/Projet_RI/docs/docs/', a)
+
+#     print("Dice coef", v.diceCoef())
+#     print("Cos mesure", v.cosinusMesure())
+#     print("Jaccard mesure", v.jaccardMesure())
+#     print("inner product", v.innerProduct())
+
+
+
+    
