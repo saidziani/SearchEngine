2e5169c5b62a2de3d1e2a4a715755a1875bad363
Said <saidziani.pro@gmail.com>
2017-12-05 10:41:33 +0100

    add new link, 'Evaluation' to open EvaluationSpace.

--- Application.py
+++ Application.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-import SearchSpaceBool, SearchSpaceVec
+import SearchSpaceBool, SearchSpaceVec, EvaluationSpace
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -15,14 +15,14 @@ class Ui_Form(object):
         self.hideWindow = Form.hide
 
         self.label = QtWidgets.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(270, 330, 150, 51))
+        self.label.setGeometry(QtCore.QRect(150, 330, 150, 51))
         font = QtGui.QFont()
         font.setFamily("Lato")
         font.setPointSize(15)
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(Form)
-        self.label_2.setGeometry(QtCore.QRect(430, 330, 150, 51))
+        self.label_2.setGeometry(QtCore.QRect(530, 330, 150, 51))
         font = QtGui.QFont()
         font.setFamily("Lato")
         font.setPointSize(15)
@@ -30,16 +30,29 @@ class Ui_Form(object):
         self.label_2.setObjectName("label_2")
 
         self.label_3 = QtWidgets.QLabel(Form)
-        self.label_3.setGeometry(QtCore.QRect(380, 330, 21, 51))
+        self.label_3.setGeometry(QtCore.QRect(275, 330, 21, 51))
+
+        self.label_7 = QtWidgets.QLabel(Form)
+        self.label_7.setGeometry(QtCore.QRect(465, 330, 21, 51))
+
+        self.label_6 = QtWidgets.QLabel(Form)
+        self.label_6.setGeometry(QtCore.QRect(340, 330, 100, 51))
+        self.label_6.setFont(font)
+        self.label_6.setObjectName("label_6")
+        self.label_6.setStyleSheet("font-weight:600")
+        self.label_6.mousePressEvent = self.openEvaluation
+
+        # self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        # self.label_7.setFont(font)
+        self.label_7.setObjectName("label_7")
 
         font = QtGui.QFont()
         font.setFamily("Lato")
         font.setPointSize(32)
         font.setBold(True)
         font.setWeight(75)
-        self.label_3.setFont(font)
-        self.label_3.setStyleSheet("color: #bdbdbd;")
-        self.label_3.setObjectName("label_3")
+        
         self.label_4 = QtWidgets.QLabel(Form)
         self.label_4.setText("")
         self.label_4.setObjectName("label_4")
@@ -54,6 +67,7 @@ class Ui_Form(object):
         self.label_2.setStyleSheet("font-weight:600")
         self.label.setStyleSheet("font-weight:600")
 
+
         self.label_5 = QtWidgets.QLabel(Form)
         self.label_5.setGeometry(QtCore.QRect(20, 20, 67, 17))
         self.label_5.setObjectName("label_5")
@@ -63,6 +77,7 @@ class Ui_Form(object):
         self.label_5.setGeometry(QtCore.QRect(25, 15, 20, 20))
         self.label_5.mousePressEvent = self.retour
 
+
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
@@ -71,12 +86,14 @@ class Ui_Form(object):
         Form.setWindowTitle(_translate("Form", "MonChef."))
         self.label.setText(_translate("Form", "Recherche\nBooléenne"))
         self.label_2.setText(_translate("Form", "Recherche\nVectorielle"))
+        self.label_6.setText(_translate("Form", "Évaluation"))
         self.label_3.setText(_translate("Form", " |"))
+        self.label_7.setText(_translate("Form", " |"))
+
         self.label_5.setText(_translate("Form", ""))
 
 
     def openBoolean(self, event):
-        print('Boolean')
         self.window = QtWidgets.QMainWindow()
         self.ui = SearchSpaceBool.SearchSpaceBool()
         self.ui.setupUi(self.window)
@@ -84,14 +101,19 @@ class Ui_Form(object):
 
 
     def openVectorial(self, event):
-        print('ARABE')
         self.window = QtWidgets.QWidget()
         self.ui = SearchSpaceVec.SearchSpaceVec()
         self.ui.setupUi(self.window)
         self.window.show()
 
+    def openEvaluation(self, event):
+        self.window = QtWidgets.QWidget()
+        self.ui = EvaluationSpace.EvaluationSpace()
+        self.ui.setupUi(self.window)
+        self.window.show()
+
+
     def retour(self, event):
-        print('BACK')
         Form.close()
 
 
